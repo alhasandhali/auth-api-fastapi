6f8372dbd529e2161bc96962b71466bed2ad486e
Al Hasan Dhali <alhasandhali@gmail.com>
2026-07-07 19:16:30 +0600

    feat: Increase access token expiry time to 24 hours

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ load_dotenv()
 SECRET_KEY: str = os.getenv("SECRET_KEY", "mysecretkey123456789")
 ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
-    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "1440")
 )
